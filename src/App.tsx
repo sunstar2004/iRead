21932912e4095630539bb3264e8a5e84b9b6c6fb
sunxing <sunxing@jiuaoedu.com>
2026-01-30 17:59:48 +0800

    chore(project): 初始化项目结构和配置文件
    添加了基本的项目配置文件，包括：
    - .gitignore: 配置忽略文件和目录
    - PROJECT_STRUCTURE.md: 详细的项目目录结构说明文档
    - README.md: 项目介绍和开发指南
    - eslint.config.js: ESLint配置文件
    - index.html: HTML模板文件

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import bookLogo from '../public/vite.svg'
+import readerLogo from './assets/react.svg'
+function App() {
+  return (
+    <>
+      <div></div>
+    </>
+  )
+}
+
+export default App
